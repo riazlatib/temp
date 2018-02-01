957289a0f6f911eeca861e4fbfd76bad90d70c98
Riaz Latib <riazlatib@gmail.com>
2018-01-31 22:29:49 -0500

    init data
    Setup the JSON file

--- js/app.js
+++ js/app.js
@@ -1,15 +1,7 @@
-console.log('Charting temps...');
+// Get data from data/data.json file
 
-var data = {
-  // A labels array that can contain any sort of values
-  labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-  // Our series array that contains series objects or in this case series data arrays
-  series: [
-    [5, 2, 4, 2, 0]
-  ]
-};
-
-// Create a new line chart object where as first parameter we pass in a selector
-// that is resolving to our chart container element. The Second parameter
-// is the actual data object.
-new Chartist.Line('.ct-chart', data);
+fetch('../data/data.json').then(function(response) {
+  return response.json();
+}).then(function(data) {
+  new Chartist.Line('.ct-chart', data);
+});
